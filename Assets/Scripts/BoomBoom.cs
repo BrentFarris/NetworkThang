b0a3106c34a59c71c44f6a35465beebf4da038fe
Brent Farris <brent@farrisarts.com>
2015-04-11 16:54:18 -0700

    Net unity update

---
+++ Assets/Scripts/BoomBoom.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomBoom : MonoBehaviour
+{
+	[RPC]
+	private void ScaleUp(float amount, string newName)
+	{
+		gameObject.name = "I'm bigger cause " + newName;
+		transform.localScale += Vector3.one * amount;
+	}
+
+	private void Update()
+	{
+		if (!networkView.isMine) return;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			networkView.RPC("ScaleUp", RPCMode.All, 1.25f, "I'mma badass");
+	}
+
+	//private void Start()
+	//{
+	//	ImCoo("Brent", "Brent", "Brent");
+	//}
+
+	//private void ImCoo(params string[] names)
+	//{
+	//	foreach (string n in names)
+	//		Debug.Log(n);
+	//}
+}
