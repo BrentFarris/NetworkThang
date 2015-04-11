fb4cd76a414178eae088bc951dd5dd8babd4f804
Brent Farris <brent@farrisarts.com>
2015-04-11 16:18:57 -0700

    Initial code content commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour
+{
+	public float networkInterpolationSpeed = 0.1f;
+	private Vector3 finalDestination = Vector3.zero;
+	private Quaternion finalRotation = Quaternion.identity;
+	private Vector3 finalScale = Vector3.zero;
+
+	private void Start()
+	{
+		finalDestination = transform.position;
+		finalRotation = transform.rotation;
+		finalScale = transform.localScale;
+	}
+
+	private void Update()
+	{
+		if (!networkView.isMine)
+		{
+			transform.position = Vector3.Lerp(transform.position, finalDestination, networkInterpolationSpeed);
+			transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, networkInterpolationSpeed);
+			transform.localScale = Vector3.Lerp(transform.localScale, finalScale, networkInterpolationSpeed);
+		}
+	}
+
+	private void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			Vector3 currentPosition = transform.position;
+			Quaternion currentRotation = transform.rotation;
+			Vector3 currentScale = transform.localScale;
+
+			stream.Serialize(ref currentPosition);
+			stream.Serialize(ref currentRotation);
+			stream.Serialize(ref currentScale);
+		}
+		else
+		{
+			stream.Serialize(ref finalDestination);
+			stream.Serialize(ref finalRotation);
+			stream.Serialize(ref finalScale);
+		}
+	}
+}
---
+++ Assets/Scripts/NetworkMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMenu : MonoBehaviour
+{
+	public string host = "127.0.0.1";
+	public int port = 9999;
+	public bool connected = false;
+
+	private void OnGUI()
+	{
+		if (connected)
+		{
+			if (Network.isServer)
+				GUILayout.Label("Connections: " + Network.connections.Length);
+
+			return;
+		}
+
+		if (GUILayout.Button("Host"))
+		{
+			NetworkConnectionError error = Network.InitializeServer(32, port, true);
+			if (error != NetworkConnectionError.NoError)
+			{
+				Debug.LogError(error.ToString());
+				return;
+			}
+
+			connected = true;
+		}
+
+		if (GUILayout.Button("Connect"))
+		{
+			NetworkConnectionError error = Network.Connect(host, port);
+			if (error != NetworkConnectionError.NoError)
+			{
+				Debug.LogError(error.ToString());
+				return;
+			}
+
+			connected = true;
+		}
+	}
+}
